e3f0bccc148ce793e131fffcfb35197a131bbdb5
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-08-10 22:30:27 +0200

    created header search box render logic to render on pages needing search algorithim

--- src/features/home/shared/Top.tsx
+++ src/features/home/shared/Top.tsx
@@ -1,19 +1,45 @@
-//import logo from "/assets/school-logo.png";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 function Top(): React.ReactElement {
+  const [searchParams, _] = useSearchParams();
+  const sections = searchParams.get("section");
+  const [renderSearchBox, setRenderSearchBox] = useState<boolean>(false);
+  //
+  useEffect(() => {
+    switch (sections) {
+      case "students/alerts":
+        setRenderSearchBox(true);
+        break;
+      case "students/info":
+        setRenderSearchBox(true);
+        break;
+      case "students/feedback/reports":
+        setRenderSearchBox(true);
+        break;
+      case "students/tracking":
+        setRenderSearchBox(true);
+        break;
+      default:
+        setRenderSearchBox(false);
+        break;
+    }
+  }, [sections]);
   return (
-    <header className="w-full h-fit bg-pramary-dark-blue component-spacing flex items-center">
+    <header className="w-full  bg-pramary-dark-blue component-spacing flex items-center h-14">
       <section className="text-text-color mr-auto flex gap-1 items-center">
         <h1 className="font-extrabold ">
           GSS KARSHI STUDENTS RECORD & TRACKING
         </h1>
       </section>
-      <search className="w-60 bg-body-color h-8 mr-auto rounded-xs flex items-center gap-1.5 pl-1">
-        <i className="fa fa-search text-gray-200 text-xl"></i>
-        <input
-          className="w-full h-full pl-1 placeholder:text-gray-400"
-          placeholder="Search..."
-        ></input>
-      </search>
+      {renderSearchBox && (
+        <search className="w-60 bg-body-color h-8 mr-auto rounded-xs flex items-center gap-1.5 pl-1">
+          <i className="fa fa-search text-gray-200 text-xl"></i>
+          <input
+            className="w-full h-full pl-1 placeholder:text-gray-400"
+            placeholder="Search..."
+          ></input>
+        </search>
+      )}
     </header>
   );
 }
