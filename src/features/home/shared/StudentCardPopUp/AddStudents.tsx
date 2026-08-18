1502fb046f124c7c502d62c0233b0f42bec9ee22
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-08-18 05:52:11 +0200

    added loading animation on tracking id generation

--- src/features/home/shared/StudentCardPopUp/AddStudents.tsx
+++ src/features/home/shared/StudentCardPopUp/AddStudents.tsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import StudentInfo from "./components/StudentInfo";
 import DayOrBoadingOption from "./components/DayOrBoadingOption";
+import loadingAnimationLine from "/assets/loading-line.gif";
 function AddStudents({ control }: { control: () => void }): React.ReactElement {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  function getTrackingId(): void {
+    setIsLoading(true);
+  }
   return (
     <article className="w-full h-screen transition-all bg-[#63606027] rounded-xl component-spacing absolute top-0 z-10 ">
       <section className="w-full h-full flex justify-center items-center">
@@ -25,20 +31,27 @@ function AddStudents({ control }: { control: () => void }): React.ReactElement {
                   <h5 className="text-[20px] font-sans font-semibold">
                     Tracking ID
                   </h5>
-                  <h5 className="font-sans font-medium text-[16px]">
-                    ***************************
-                  </h5>
+                  {isLoading ? (
+                    <img className="w-75 h-20" src={loadingAnimationLine}></img>
+                  ) : (
+                    <h5 className="font-sans font-medium text-[16px]">
+                      ***************************
+                    </h5>
+                  )}
                 </span>
                 {/**action button */}
                 <div className="flex justify-center mt-2">
-                  {true ? (
+                  {!true ? (
                     <button className="w-fit h-fit p-4 rounded-3xl  bg-[#089408] hover:bg-[#51df51] transition-all pointer text-center button-shadow">
                       <h5 className="text-[16px] font-sans font-semibold text-text-color">
                         Save Data
                       </h5>
                     </button>
                   ) : (
-                    <button className="w-fit h-fit p-4 rounded-3xl  bg-[#4646d1] hover:bg-[#9b9beb] transition-all pointer text-center">
+                    <button
+                      className="w-fit h-fit p-4 rounded-3xl  bg-[#4646d1] hover:bg-[#9b9beb] transition-all pointer text-center button-shadow"
+                      onClick={getTrackingId}
+                    >
                       <h5 className="text-[16px] font-sans font-semibold text-text-color">
                         Generate ID
                       </h5>
