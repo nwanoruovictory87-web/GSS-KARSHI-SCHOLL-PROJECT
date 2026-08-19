95f671766b89f7d8cce866087fca9ec0ce03d909
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-08-19 19:34:26 +0200

    added get all students record end points and render students info function flow

--- src/features/home/features/studentsRecord/components/List.tsx
+++ src/features/home/features/studentsRecord/components/List.tsx
@@ -1,24 +1,58 @@
+import { useState, useEffect } from "react";
 import StudentsInfo from "./StudentsInfo";
+import { getStudentsRecords } from "../api/StudentsRecordApi";
+interface StudentsData {
+  _id: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  age: string;
+  dateOfBirth: string;
+  gender: string;
+  house: string;
+  dayStudent: number;
+  bordingStudent: number;
+  trackingID: string;
+  image: null;
+  studentYear: string;
+  createdAt: Date;
+  __v: number;
+}
 function List(): React.ReactElement {
+  const [studentsRecordsList, setStudentsRecirdList] = useState<StudentsData[]>(
+    [],
+  );
+  //
+  useEffect(() => {
+    getStudentsRecords()
+      .then((data) => {
+        setStudentsRecirdList(data.records);
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  //
   return (
-    <div className=" w-full h-full overflow-y-auto pb-12 ">
+    <div className=" w-full h-full overflow-y-scroll pb-12 ">
       {/** */}
       <div className="w-full flex flex-col  border-r border-r-body-color border-l border-l-body-coborder-r-body-color ">
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
-        <StudentsInfo />
+        {studentsRecordsList.map((data: StudentsData) => {
+          return (
+            <StudentsInfo
+              image={data.image}
+              firstName={data.firstName}
+              middleName={data.middleName}
+              lastName={data.lastName}
+              house={data.house}
+              trackingID={data.trackingID}
+              gender={data.gender}
+              year={data.studentYear}
+              key={data.trackingID}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/features/home/features/studentsRecord/components/StudentsInfo.tsx
+++ src/features/home/features/studentsRecord/components/StudentsInfo.tsx
@@ -1,30 +1,48 @@
-function StudentsInfo(): React.ReactElement {
+function StudentsInfo({
+  image,
+  firstName,
+  middleName,
+  lastName,
+  trackingID,
+  gender,
+  house,
+  year,
+}: {
+  image: string | null;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  trackingID: string;
+  gender: string;
+  house: string;
+  year: string;
+}): React.ReactElement {
   return (
     <section className="w-full h-15  grid grid-cols-[28%_25%_12%_10%_10%_15%] border border-body-color">
       <span className="flex gap-4 pl-2 p-1 items-center font-medium border-r border-body-color ">
         <span className="min-w-13 min-h-13 max-w-13 max-h-13 rounded-full bg-gray-200"></span>
         <h5 className="text-text-color font-medium min16Max18px line-clamp-1">
-          Victory Nwanoruo
+          {firstName} {middleName} {lastName}
         </h5>
       </span>
       <span className="flex pl-2  items-center font-medium border-r border-body-color">
         <h5 className="text-text-color font-medium min16Max18px line-clamp-1">
-          7348b7c337btv357n,8u
+          {trackingID}
         </h5>
       </span>
       <span className="flex pl-2  items-center font-medium border-r border-body-color">
         <h5 className="text-text-color font-medium min16Max18px line-clamp-1">
-          Male
+          {gender}
         </h5>
       </span>
       <span className="flex pl-2  items-center font-medium border-r border-body-color">
         <h5 className="text-text-color font-medium min16Max18px line-clamp-1">
-          Garki
+          {house}
         </h5>
       </span>
       <span className="flex pl-2  items-center font-medium border-r border-body-color">
         <h5 className="text-text-color font-medium min16Max18px line-clamp-1">
-          2022
+          {year}
         </h5>
       </span>
       <span className="grid grid-cols-3 gap-1 pl-2 pr-2 text-text-color  items-center font-medium ">
