38b862693bd71f6c394953e22462d083ecc6fe04
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-08-20 03:14:34 +0100

    created students record storage to store students data as to not send a http payload on page remount

---
+++ src/storage/StudentsRecord.tsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from "react";
+interface StudentsData {
+  _id: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  age: string;
+  dateOfBirth: string;
+  gender: string;
+  house: string;
+  year: string;
+  dayStudent: number;
+  bordingStudent: number;
+  trackingID: string;
+  image: null;
+  studentYear: string;
+  createdAt: Date;
+  __v: number;
+}
+interface Records {
+  records: StudentsData[] | [];
+  setRecords: React.Dispatch<React.SetStateAction<StudentsData[]>>;
+}
+const StudentsContextData = createContext<Records>({
+  records: [],
+  setRecords: () => {},
+});
+export function StudentsRecordProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [records, setRecords] = useState<StudentsData[] | []>([]);
+  return (
+    <StudentsContextData.Provider
+      value={{
+        records,
+        setRecords,
+      }}
+    >
+      {children}
+    </StudentsContextData.Provider>
+  );
+}
+export const StudentsRecordStorage = () => useContext(StudentsContextData);
