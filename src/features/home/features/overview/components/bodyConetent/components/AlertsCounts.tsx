e46bacdcf6b7960cdecaaf2e33e4879c3f49fed4
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-08-22 23:41:19 +0100

    implemented in alert counts content prop for dynamic page render also created loading animation

--- src/features/home/features/overview/components/bodyConetent/components/AlertsCounts.tsx
+++ src/features/home/features/overview/components/bodyConetent/components/AlertsCounts.tsx
@@ -1,4 +1,12 @@
-function AlertsCounts(): React.ReactElement {
+function AlertsCounts({
+  panicCount,
+  warningCount,
+  stableCount,
+}: {
+  panicCount: number | null;
+  warningCount: number | null;
+  stableCount: number | null;
+}): React.ReactElement {
   return (
     <section className="w-full h-[38%] flex flex-col overflow-hidden font-sans bg-pramary-dark-blue rounded-xl mb-auto ">
       <div className="w-full pl-px pr-px">
@@ -7,23 +15,41 @@ function AlertsCounts(): React.ReactElement {
         </span>
       </div>
       <div className="w-full h-full pl-2 pr-2 flex flex-col gap-3 mt-3   text-text-color font-medium">
-        <span className="grid grid-cols-[70%_30%]  gap-2 ">
+        <span className="grid grid-cols-[70%_30%] gap-2 pr-2 items-center">
           <h5 className="min18Max22px font-medium border-r border-body-color line-clamp-1">
             Today's panic count
           </h5>
-          <h5 className="min18Max22px font-bold text-[#ff0000d2]">23</h5>
+          {panicCount ? (
+            <h5 className="min18Max22px font-bold text-[#ff0000d2]">
+              {panicCount}
+            </h5>
+          ) : (
+            <h5 className="min18Max22px font-bold w-full h-4 tr-skeleton"></h5>
+          )}
         </span>
-        <span className="grid grid-cols-[70%_30%]  gap-2 ">
+        <span className="grid grid-cols-[70%_30%] gap-2 pr-2 items-center">
           <h5 className="min18Max22px font-medium border-r border-body-color line-clamp-1">
             Today's warning count
           </h5>
-          <h5 className="min18Max22px font-bold text-[#ffa600e5]">242</h5>
+          {warningCount ? (
+            <h5 className="min18Max22px font-bold text-[#ffa600e5]">
+              {warningCount}
+            </h5>
+          ) : (
+            <h5 className="min18Max22px font-bold w-full h-4 tr-skeleton"></h5>
+          )}
         </span>
-        <span className="grid grid-cols-[70%_30%]  gap-2 ">
+        <span className="grid grid-cols-[70%_30%] gap-2 pr-2 items-center">
           <h5 className="min18Max22px font-medium border-r border-body-color line-clamp-1">
             Total stable count
           </h5>
-          <h5 className="min18Max22px font-bold text-[#0da70dd3]">2432</h5>
+          {warningCount ? (
+            <h5 className="min18Max22px font-bold text-[#0da70dd3]">
+              {stableCount}
+            </h5>
+          ) : (
+            <h5 className="min18Max22px font-bold w-full h-4 tr-skeleton"></h5>
+          )}
         </span>
       </div>
     </section>
