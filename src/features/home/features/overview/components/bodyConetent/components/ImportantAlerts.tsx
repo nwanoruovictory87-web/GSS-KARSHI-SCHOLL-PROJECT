ea681b1680af1e4fb97661e0c494dd3b80517044
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-08-22 21:35:53 +0100

    implemention import alerts prop to importent card for dynamic render also created loading animation

--- src/features/home/features/overview/components/bodyConetent/components/ImportantAlerts.tsx
+++ src/features/home/features/overview/components/bodyConetent/components/ImportantAlerts.tsx
@@ -12,65 +12,103 @@ function ImportantAlerts(): React.ReactElement {
         </span>
       </div>
       <div className="w-full h-full pl-2 pr-2 grid grid-rows-5 ">
-        <ImportantAlertsCard />
-        {/** */}
-        <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
-          <span className="border-r border-body-color flex items-center">
-            <h5 className="line-clamp-1">7348b7c337btv357n,8u</h5>
-          </span>
-          <span className="justify-center border-r border-body-color flex items-center ">
-            <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl bg-[#ffa600e5]">
-              <i className="fa fa-triangle-exclamation"></i>
-            </span>
-          </span>
-          <span className="border-r border-body-color flex items-center pl-1">
-            <h5>2PM</h5>
-          </span>
-          <span className=" flex items-center justify-center ">
-            <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl bg-[#4b4be9]">
-              <i className="fa fa-location"></i>
-            </span>
-          </span>
-        </div>
-        <ImportantAlertsCard />
-        {/** */}
-        <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
-          <span className="border-r border-body-color flex items-center">
-            <h5 className="line-clamp-1">7348b7c337btv357n,8u</h5>
-          </span>
-          <span className="justify-center border-r border-body-color flex items-center ">
-            <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl bg-[#ffa600e5]">
-              <i className="fa fa-triangle-exclamation"></i>
-            </span>
-          </span>
-          <span className="border-r border-body-color flex items-center pl-1">
-            <h5>2PM</h5>
-          </span>
-          <span className=" flex items-center justify-center ">
-            <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl bg-[#4b4be9]">
-              <i className="fa fa-location"></i>
-            </span>
-          </span>
-        </div>
-        {/** */}
-        <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
-          <span className="border-r border-body-color flex items-center">
-            <h5 className="line-clamp-1">7348b7c337btv357n,8u</h5>
-          </span>
-          <span className="justify-center border-r border-body-color flex items-center ">
-            <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl bg-[#ffa600e5]">
-              <i className="fa fa-triangle-exclamation"></i>
-            </span>
-          </span>
-          <span className="border-r border-body-color flex items-center pl-1">
-            <h5>2PM</h5>
-          </span>
-          <span className=" flex items-center justify-center ">
-            <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl bg-[#4b4be9]">
-              <i className="fa fa-location"></i>
-            </span>
-          </span>
-        </div>
+        {!true ? (
+          <ImportantAlertsCard />
+        ) : (
+          <>
+            <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
+              <span className="border-r border-body-color pl-2 pr-2 flex items-center">
+                <h5 className="line-clamp-1  w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className="justify-center border-r border-body-color flex items-center ">
+                <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl tr-skeleton">
+                  <i className="fa fa-triangle-exclamation tr-skeleton-text"></i>
+                </span>
+              </span>
+              <span className="border-r border-body-color flex items-center pl-2 pr-2">
+                <h5 className=" w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className=" flex items-center justify-center ">
+                <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl   tr-skeleton">
+                  <i className="fa fa-location tr-skeleton-text"></i>
+                </span>
+              </span>
+            </div>
+            <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
+              <span className="border-r border-body-color pl-2 pr-2 flex items-center">
+                <h5 className="line-clamp-1  w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className="justify-center border-r border-body-color flex items-center ">
+                <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl tr-skeleton">
+                  <i className="fa fa-triangle-exclamation tr-skeleton-text"></i>
+                </span>
+              </span>
+              <span className="border-r border-body-color flex items-center pl-2 pr-2">
+                <h5 className=" w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className=" flex items-center justify-center ">
+                <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl   tr-skeleton">
+                  <i className="fa fa-location tr-skeleton-text"></i>
+                </span>
+              </span>
+            </div>
+            <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
+              <span className="border-r border-body-color pl-2 pr-2 flex items-center">
+                <h5 className="line-clamp-1  w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className="justify-center border-r border-body-color flex items-center ">
+                <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl tr-skeleton">
+                  <i className="fa fa-triangle-exclamation tr-skeleton-text"></i>
+                </span>
+              </span>
+              <span className="border-r border-body-color flex items-center pl-2 pr-2">
+                <h5 className=" w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className=" flex items-center justify-center ">
+                <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl   tr-skeleton">
+                  <i className="fa fa-location tr-skeleton-text"></i>
+                </span>
+              </span>
+            </div>
+            <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
+              <span className="border-r border-body-color pl-2 pr-2 flex items-center">
+                <h5 className="line-clamp-1  w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className="justify-center border-r border-body-color flex items-center ">
+                <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl tr-skeleton">
+                  <i className="fa fa-triangle-exclamation tr-skeleton-text"></i>
+                </span>
+              </span>
+              <span className="border-r border-body-color flex items-center pl-2 pr-2">
+                <h5 className=" w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className=" flex items-center justify-center ">
+                <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl   tr-skeleton">
+                  <i className="fa fa-location tr-skeleton-text"></i>
+                </span>
+              </span>
+            </div>
+            <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
+              <span className="border-r border-body-color pl-2 pr-2 flex items-center">
+                <h5 className="line-clamp-1  w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className="justify-center border-r border-body-color flex items-center ">
+                <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl tr-skeleton">
+                  <i className="fa fa-triangle-exclamation tr-skeleton-text"></i>
+                </span>
+              </span>
+              <span className="border-r border-body-color flex items-center pl-2 pr-2">
+                <h5 className=" w-full h-4 tr-skeleton"></h5>
+              </span>
+              <span className=" flex items-center justify-center ">
+                <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl   tr-skeleton">
+                  <i className="fa fa-location tr-skeleton-text"></i>
+                </span>
+              </span>
+            </div>
+          </>
+        )}
+
         {/** */}
       </div>
     </section>
--- src/features/home/features/overview/components/bodyConetent/components/ImportantAlertsCard.tsx
+++ src/features/home/features/overview/components/bodyConetent/components/ImportantAlertsCard.tsx
@@ -1,16 +1,31 @@
-function ImportantAlertsCard(): React.ReactElement {
+function ImportantAlertsCard({
+  trackingID,
+  trackingState,
+  watchTime,
+}: {
+  trackingID: string;
+  watchTime: string;
+  trackingState: number;
+  lastTransmistedDate: string;
+}): React.ReactElement {
   return (
     <div className="w-full h-12 text-text-color  grid grid-cols-[55%_15%_15%_15%] ">
       <span className="border-r border-body-color flex items-center">
-        <h5 className="line-clamp-1">7348b7c337btv357n,8u</h5>
+        <h5 className="line-clamp-1">{trackingID}</h5>
       </span>
       <span className="justify-center border-r border-body-color flex items-center ">
-        <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl bg-[#ff0000d2]">
-          <i className="fa fa-xmark"></i>
-        </span>
+        {trackingState === 3 ? (
+          <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl bg-[#ff0000d2]">
+            <i className="fa fa-xmark"></i>
+          </span>
+        ) : (
+          <span className="flex items-center  text-black p-2 w-fit h-8 rounded-xl bg-[#ffa600e5]">
+            <i className="fa fa-triangle-exclamation"></i>
+          </span>
+        )}
       </span>
       <span className="border-r border-body-color flex items-center pl-1">
-        <h5>2PM</h5>
+        <h5>{watchTime}</h5>
       </span>
       <span className=" flex items-center justify-center ">
         <span className="flex items-center   text-black p-2 w-fit h-8 rounded-xl bg-[#4b4be9]">
