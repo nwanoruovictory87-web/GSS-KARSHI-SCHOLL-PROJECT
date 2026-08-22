0e477f55e9babc582e8aa873fea0e35d3aa0f379
victory nwanoruo <nwanoruovictory87@gmail.com>
2026-08-22 21:12:46 +0100

    creating pie chart loading animation

--- src/features/home/features/overview/components/bodyConetent/BodyContent.tsx
+++ src/features/home/features/overview/components/bodyConetent/BodyContent.tsx
@@ -13,7 +13,19 @@ function BodyContent() {
           </span>
         </div>
         <section className="w-full h-full  flex justify-center items-center p-2 text-text-color">
-          <StudentsLineGraph />
+          {!true ? (
+            <StudentsLineGraph total={0} />
+          ) : (
+            <div className="w-[80%] h-[80%] flex flex-col items-center gap-2">
+              <div className="w-fit flex gap-5">
+                <span className="block w-12 h-5 tr-skeleton"></span>
+                <span className="block w-12 h-5 tr-skeleton"></span>
+                <span className="block w-12 h-5 tr-skeleton"></span>
+                <span className="block w-12 h-5 tr-skeleton"></span>
+              </div>
+              <div className="w-87.5 h-87.5 rounded-full tr-skeleton"></div>
+            </div>
+          )}
         </section>
       </article>
       <article className="w-[38%] h-full flex flex-col  ml-auto">
